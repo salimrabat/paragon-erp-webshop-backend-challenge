ae43fcd682e75acd90e4017a6b461a54d870d819
Salim Rabat <salim.rabat@tendium.com>
2023-02-25 11:27:24 +0100

    initialize db handler instance outside of endpoint

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const PostgresqlDBHandler = require("../database/queries");
 const router = express.Router();
+const dataBase = new PostgresqlDBHandler()
 
 
 router.get('/', async (req, res) => {
-    await new PostgresqlDBHandler().testConnection()
+    await dataBase.testConnection()
         .then(_ => {
             res.status(200).json({
                 message: "Connection working"
@@ -18,8 +19,8 @@ router.get('/', async (req, res) => {
 /**
  * Api request to get all users in the database.
  */
-router.get('/users', async (req, res) => {
-    await new PostgresqlDBHandler().getAllUsers()
+router.get('/users', async (req, res) =>
+    await dataBase.getAllUsers()
         .then(users => {
             res.status(200).json({
                 users: users
@@ -28,21 +29,29 @@ router.get('/users', async (req, res) => {
         .catch(reason => {
             console.log('Could not fetch all users from database', reason);
         })
-})
+)
 
 /**
  * Api request to get user by id.
  */
 router.get('/user', async (req, res) => {
-    await new PostgresqlDBHandler().getUserById(req.query.id)
-        .then(user => {
+    try {
+        const user = await dataBase.getUserById(req.query.id)
+        if (user != null) {
             res.status(200).json({
                 user: user
             });
+        } else {
+            res.status(204).json({
+                message: "User doesn't exist"
+            });
+        }
+
+    } catch (reason) {
+        console.log('Could not fetch user from database', reason);
+        res.status(500).json({
         })
-        .catch(reason => {
-            console.log('Could not fetch user from database', reason);
-        })
+    }
 })
 
 
@@ -50,7 +59,7 @@ router.get('/user', async (req, res) => {
  * Api request to get all products in the database.
  */
 router.get('/products', async (req, res) => {
-    await new PostgresqlDBHandler().getAllProducts()
+    await dataBase.getAllProducts()
         .then(products => {
             res.status(200).json({
                 products: products
@@ -72,7 +81,6 @@ router.get('/products', async (req, res) => {
  * @param {Array} products
  */
 router.post('/order', async (req, res) => {
-    const pgHandler = new PostgresqlDBHandler()
     const userId = req.body.userId
     let streetAddress = req.body.streetAddress
     let postalCode = req.body.postalCode
@@ -80,7 +88,7 @@ router.post('/order', async (req, res) => {
     let country = req.body.country
     let products = req.body.products
 
-    let user = await pgHandler.getUserById(userId)
+    let user = await dataBase.getUserById(userId)
     if (user === null || user === undefined) {
         return res.status(200).json({
             message: "User does not exist",
@@ -89,7 +97,7 @@ router.post('/order', async (req, res) => {
     }
     for (let i = 0; i < products.length; i++) {
         let product = products[i]
-        let result = await pgHandler.getProductByIdAndCheckIfInStock(product.id, product.quantity)
+        let result = await dataBase.getProductByIdAndCheckIfInStock(product.id, product.quantity)
         if (result === null || result === undefined) {
             return res.status(200).json({
                 message: "Product out of stock",
@@ -97,7 +105,7 @@ router.post('/order', async (req, res) => {
             });
         }
     }
-    await pgHandler.createOrder(userId, streetAddress, postalCode, city, country, products)
+    await dataBase.createOrder(userId, streetAddress, postalCode, city, country, products)
         .then(orderId => {
             res.status(200).json({
                 orderId: orderId,
